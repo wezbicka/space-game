4bcfcd9b93dcd8eb41bc99b36109581502e0b578
Polina <75391420+wezbicka@users.noreply.github.com>
2023-06-25 15:06:48 +0300

    Render 5 stars

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
 import time
+import asyncio
+
 import curses
-import random
+
 
 def draw(canvas):
-    
-    stars = []
-    x = curses.LINES
-    y = curses.COLS
-    for i in range(10):
-        star_x = random.randint(1, x)
-        star_y = random.randint(1, y)
-        kord = (star_x, star_y)
-        stars.append(kord)
-
     canvas.border()
-    delay = (2, 0.3, 0.5, 0.3)
-    count = 0
+    coroutines = [blink(canvas, 5, number * 5) for number in range(1, 6)]
     while True:
-        
-        for i in stars:
-            
-            if count % 4 == 0:
-                canvas.addstr(i[0], i[1], '*', curses.A_DIM)
-            elif count % 4 == 2:
-                canvas.addstr(i[0], i[1], '*', curses.A_BOLD)
-            else:
-                canvas.addstr(i[0], i[1], '*',)
-        curses.curs_set(False)       
-        count += 1
+        for coroutine in coroutines.copy():
+            try:
+                coroutine.send(None)
+            except StopIteration:
+                coroutines.remove(coroutine)
         canvas.refresh()
-        time.sleep(delay[count % 4])
+        time.sleep(1)
 
   
+async def blink(canvas, row, column, symbol='*'):
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+
 if __name__ == '__main__':
     curses.update_lines_cols()
     curses.wrapper(draw)
