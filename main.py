6fea2b97b5c0aa7b7c40e45a44dfc9232454ab21
Polina <75391420+wezbicka@users.noreply.github.com>
2023-09-22 14:05:27 +0300

    Replace numbers to variables. Optimize screen work

--- main.py
+++ main.py
@@ -24,6 +24,25 @@ def get_frames(folder="spaceship"):
     return frames
 
 
+def get_playground_limits(canvas, with_border=True):
+    max_row, max_col = canvas.getmaxyx()
+    canvas_padding = 2
+    if with_border:
+        min_row, min_col = (1, 1)
+        max_row -= canvas_padding
+        max_col -= canvas_padding
+    else:
+        min_row, min_col = (0, 0)
+    return min_row, min_col, max_row, max_col
+
+
+def get_random_coords(min_row, max_row, min_col, max_col):
+    return (
+        random.randint(min_row, max_row),
+        random.randint(min_col, max_col),
+    )
+
+
 def draw(canvas):
     canvas.nodelay(True)
     symbols = "*.○+●°•☆:☼★٭✽❇❈❉❊❋⁂"
@@ -33,12 +52,12 @@ def draw(canvas):
     coroutines = []
     frames = get_frames()
     coroutines.append(animate_spaceship(canvas, window_y//2, window_x//2, frames))
-
-    for i in range(100):
+    stars_count = 100
+    min_row, min_col, max_row, max_col = get_playground_limits(canvas)
+    for i in range(stars_count):
         coroutines.append(blink(
             canvas,
-            random.randint(1, window_y-5),
-            random.randint(1, window_x-5),
+            *get_random_coords(min_row, max_row, min_col, max_col),
             random.randint(0, 3),
             random.choice(symbols),
         ))
@@ -61,7 +80,7 @@ def draw(canvas):
 
 async def animate_spaceship(canvas, row, column, frames):
     canvas.nodelay(True)
-    screen_max_y, screen_max_x = canvas.getmaxyx()
+    screen_min_y, screen_min_x, screen_max_y, screen_max_x = get_playground_limits(canvas)
     for frame in cycle(frames):
         for i in range(2):
             height, width = get_frame_size(frame) 
@@ -72,11 +91,10 @@ async def animate_spaceship(canvas, row, column, frames):
             row += x_direction
             column += y_direction
 
-            row = statistics.median([1, row, frame_max_row - 1])
-            column = statistics.median([1, column, frame_max_column - 1])
+            row = statistics.median([screen_min_y, row, frame_max_row])
+            column = statistics.median([screen_min_x, column, frame_max_column])
         
             draw_frame(canvas, row, column, frame)
-            canvas.refresh()
             await asyncio.sleep(0)
             # стираем предыдущий кадр, прежде чем рисовать новый
             draw_frame(canvas, row, column, frame, negative=True)
